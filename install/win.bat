903c9d2e30e2a46526ef86089f6ce74ac1c0fadd
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-04-20 10:50:36 -0400

    install script

---
+++ install/win.bat
@@ -0,0 +1,28 @@
+@echo off
+
+echo _
+echo Makes sure you have GIT / NPM installed
+echo Have your ssh key saved in '~/.ssh/id_rsa'
+echo _
+@pause
+echo _
+echo Installing 'switchx'...
+npm install haxeshim -g && npm install switchx -g && switchx
+switchx install latest
+echo _
+echo Adding to haxelib...
+haxelib dev openfl-build-server ..
+echo _
+echo Now add this command to the scheduler ("Log on")
+echo if you want to have it started automatically
+echo _
+echo cmd /k "cd %0\.. & start-ssh-agent & run"
+echo _
+@pause
+echo _
+echo That's pretty much it, now all you need to do
+echo is add your git projects to the 'projects' folder
+echo _
+echo git submodule add git@github.com:starburst997/openfl-sample-1.git
+echo _
+@pause
