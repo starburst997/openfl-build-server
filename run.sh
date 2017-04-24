162e626b74f39c9610461c5bfbdd9ba75c32eeda
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-04-23 21:58:03 -0400

    Test

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 haxelib dev openfl-build-server .
-#haxelib run openfl-build-server build projects
-haxelib run openfl-build-server test 2
+haxelib run openfl-build-server build projects
+#haxelib run openfl-build-server test 2
