eb18eecd3aaa2e1a10e9e33ad9c7a9c05d95eb59
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-04-28 12:10:22 -0400

    Added analytics

--- web/view.php
+++ web/view.php
@@ -225,6 +225,17 @@
 
     <!-- Custom styles for this template -->
     <link href="css/carousel.css" rel="stylesheet">
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', '<?php echo $analytics; ?>', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 	<style>
 		body{
 			font-family:'Muli','Helvetica Neue',Helvetica,Arial,sans-serif;
